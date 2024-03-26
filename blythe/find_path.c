71f56f3092d47c134653e4befd8954475d968d0f
blythelockhart <117318607+blythelockhart@users.noreply.github.com>
2024-03-26 14:46:40 -0500

    Create find_path.c

---
+++ blythe/find_path.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+ * find_path - Processes parsed commands.
+ * @cmds - Parsed commands to be processed
+ * @env - The environment containing the PATH
+ * Return: NULL.
+ */
+
+void find_path(char ***cmds, char **env)
+{
+}
