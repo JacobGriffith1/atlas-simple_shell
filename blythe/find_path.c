7d9b0d04e7df3a0d1e79d7c75d56bed060f37011
blythelockhart <117318607+blythelockhart@users.noreply.github.com>
2024-04-01 19:09:54 -0500

    Update find_path.c to add new function find_env

--- blythe/find_path.c
+++ blythe/find_path.c
@@ -7,7 +7,7 @@
  * Return: NULL.
  */
 
-void find_path(char ***cmds, char **env)
+void find_path(char ***cmds)
 {
 	int cmnd_i = 0;
 
@@ -26,29 +26,7 @@ void find_path(char ***cmds, char **env)
 				break;
 			}
 		if (!is_builtin)
-		{
-			char *path_env = getenv("PATH"), *path_copy = strdup(path_env);
-			char *path_dir = strtok(path_copy, ":");
-
-			while (path_dir != NULL)
-			{
-				char *full_path = malloc(strlen(path_dir) + strlen(command[0]) + 2);
-
-				if (full_path == NULL)
-				{
-					perror("Memory allocation failed");
-					exit(EXIT_FAILURE);
-				}
-				strcpy(full_path, path_dir);
-				strcat(full_path, "/");
-				strcat(full_path, command[0]);
-				if (access(full_path, X_OK) == 0)
-					fork_exec(command, full_path);
-				free(full_path);
-				path_dir = strtok(NULL, ":");
-			}
-			free(path_copy);
-		}
+			find_env(cmds);
 		cmnd_i++;
 	}
 }
