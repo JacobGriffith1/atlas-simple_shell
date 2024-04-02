02f509093c265aab067857ba5225328f387030ce
blythelockhart <117318607+blythelockhart@users.noreply.github.com>
2024-04-02 09:47:13 -0500

    move shell.h to proper folder

---
+++ shell.h
@@ -0,0 +1,28 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define TOK_DELIM " \t\r\n\a"
+#define TOK_BUFSIZE 64
+#define MAX_CMDS 16
+#define MAX_ARGS 64
+#define MAX_LINE_LENGTH 256
+#define MAX_BUILTINS 3
+
+int main(void);
+char ***parse(char *line);
+char **prompt();
+void fork_exec(char **command, char *full_path);
+void find_path(char ***cmds);
+void ss_cd(char **cmds);
+void ss_exit(void);
+void ss_help(void);
+void find_env(char ***cmds);
+
+#endif
